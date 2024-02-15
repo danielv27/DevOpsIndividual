842e734ea4656f1b0b9a88fcac9e45b31a000687
danielv27 <danielv2000@gmail.com>
2024-02-16 00:55:37 +0100

    mongodb implemented

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,39 +1,34 @@
 import os
 import tempfile
 from functools import reduce
+import json
+from pymongo import MongoClient
+from bson import ObjectId
 
-from tinydb import TinyDB, Query
-
-db_dir_path = tempfile.gettempdir()
-db_file_path = os.path.join(db_dir_path, "students.json")
-student_db = TinyDB(db_file_path)
+client = MongoClient("mongodb://mongo")
+db = client.db
+students = db.students
 
 
 def add(student=None):
-    queries = []
-    query = Query()
-    queries.append(query.first_name == student.first_name)
-    queries.append(query.last_name == student.last_name)
-    query = reduce(lambda a, b: a & b, queries)
-    res = student_db.search(query)
-    if res:
-        return 'already exists', 409
+    if students.find_one({"first_name": student.first_name, "last_name": student.last_name}):
+        return 'user already exists', 409
 
-    doc_id = student_db.insert(student.to_dict())
-    student.student_id = doc_id
-    return student.student_id
+    result = students.insert_one(student.to_dict())
+    return str(result.inserted_id)
 
 def get_by_id(student_id=None, subject=None):
-    student = student_db.get(doc_id=int(student_id))
+    student = students.find_one({"_id": ObjectId(student_id)})
     if not student:
-        return 'not found', 404
-    student['student_id'] = student_id
-    print(student)
+        return 'user not found', 404
+    
+    student['student_id'] = str(student['_id'])
+    del student['_id']
     return student
 
 def delete(student_id=None):
-    student = student_db.get(doc_id=int(student_id))
+    student = students.find_one({"_id": ObjectId(student_id)})
     if not student:
         return 'not found', 404
-    student_db.remove(doc_ids=[int(student_id)])
-    return student_id
+    res = students.delete_one({"_id": ObjectId(student_id)})
+    return str(student['_id'])
