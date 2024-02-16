3062b79d8cf89e236c81d2c38c1584212a5f926c
danielv27 <danielv2000@gmail.com>
2024-02-16 01:32:32 +0100

    simplified student_service

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -8,27 +8,30 @@ from bson import ObjectId
 client = MongoClient("mongodb://mongo")
 db = client.db
 students = db.students
+student_counter = 0
 
 
 def add(student=None):
     if students.find_one({"first_name": student.first_name, "last_name": student.last_name}):
         return 'user already exists', 409
 
-    result = students.insert_one(student.to_dict())
-    return str(result.inserted_id)
+    student_counter += 1
+    student['student_id'] = student_counter
+
+    students.insert_one(student.to_dict())
+    return student_counter
 
 def get_by_id(student_id=None, subject=None):
-    student = students.find_one({"_id": ObjectId(student_id)})
+    student = students.find_one({"student_id": student_id})
     if not student:
         return 'user not found', 404
     
-    student['student_id'] = str(student['_id'])
     del student['_id']
     return student
 
 def delete(student_id=None):
-    student = students.find_one({"_id": ObjectId(student_id)})
+    student = students.find_one({"student_id": student_id})
     if not student:
         return 'not found', 404
-    res = students.delete_one({"_id": ObjectId(student_id)})
-    return str(student['_id'])
+    students.delete_one({"student_id": student_id})
+    return student_id
